4561f444ac2a2d1997e142a8434de9563f475115
Naufal Rachmandani <5174397-NaufalRachmandani@users.noreply.gitlab.com>
2021-10-09 18:38:24 +0700

    update from handler to coroutine lifecycle scope with delay 2 seconds

--- app/src/main/java/com/example/jarnetor/Fragments/SplashFrag.kt
+++ app/src/main/java/com/example/jarnetor/Fragments/SplashFrag.kt
@@ -1,38 +1,44 @@
 package com.example.jarnetor.Fragments
 
 import android.os.Bundle
-import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.jarnetor.R
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class SplashFrag : Fragment() {
 
     private lateinit var mAuth: FirebaseAuth
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_splash, container, false)
 
         mAuth = FirebaseAuth.getInstance()
         val user = mAuth.currentUser
 
         //If already loggd in, send to initial frag or send to login
-        Handler().postDelayed({
+        lifecycleScope.launch(Dispatchers.Main) {
+            delay(2000)
             if (user != null) {
                 findNavController().navigate(R.id.action_splashFrag_to_subjectFrag)
             } else {
                 findNavController().navigate(R.id.action_splashFrag_to_loginFrag2)
             }
-        },200)//TODO(set this to 2000)
-
+        }
 
         return view
     }
-
 
 }
